49ecb075ff7b84de360aa540c638f17d7a2e899d
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-24 00:41:27 +0530

    Add Settings component with styling

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,22 @@
+export default function Settings(){
+
+    return(
+
+        <div
+            style={{
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                height:"100%",
+                color:"white",
+                fontSize:"40px"
+            }}
+        >
+
+            ⚙ Settings
+
+        </div>
+
+    );
+
+}
