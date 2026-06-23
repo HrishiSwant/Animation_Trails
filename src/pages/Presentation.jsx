88b9219d87fad2b4c92d56f3b23bd0f506c231b1
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-24 00:40:58 +0530

    Create Presentation.jsx

---
+++ src/pages/Presentation.jsx
@@ -0,0 +1,22 @@
+export default function Presentation(){
+
+    return(
+
+        <div
+            style={{
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                height:"100%",
+                color:"white",
+                fontSize:"40px"
+            }}
+        >
+
+            🎭 Presentation Mode
+
+        </div>
+
+    );
+
+}
