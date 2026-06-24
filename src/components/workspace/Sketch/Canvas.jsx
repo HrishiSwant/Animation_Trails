7f110f7983ae891b57b15ea8f946277e70bd10a2
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-24 18:24:18 +0530

    Add Canvas component with basic structure

---
+++ src/components/workspace/Sketch/Canvas.jsx
@@ -0,0 +1,19 @@
+import "./Canvas.css";
+
+export default function Canvas() {
+
+  return (
+
+    <div className="canvas">
+
+      <div className="canvas-message">
+
+        Canvas Engine
+
+      </div>
+
+    </div>
+
+  );
+
+}
