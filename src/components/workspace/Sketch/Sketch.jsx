d7b2a485521cca87031f641e0f8128290627a664
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-24 18:26:47 +0530

    Add Sketch component with Toolbar and Canvas

---
+++ src/components/workspace/Sketch/Sketch.jsx
@@ -0,0 +1,20 @@
+import "./Sketch.css";
+
+import Toolbar from "./Toolbar";
+import Canvas from "./Canvas";
+
+export default function Sketch(){
+
+    return(
+
+        <div className="sketch">
+
+            <Toolbar/>
+
+            <Canvas/>
+
+        </div>
+
+    );
+
+}
