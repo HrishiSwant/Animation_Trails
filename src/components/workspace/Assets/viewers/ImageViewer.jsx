2bb868d22924863cd40e36c82237b3c25913ab30
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-25 18:01:51 +0530

    Implement ImageViewer component with interactive features

--- src/components/workspace/Assets/viewers/ImageViewer.jsx
+++ src/components/workspace/Assets/viewers/ImageViewer.jsx
@@ -1,1 +1,75 @@
+export default function ImageViewer({
 
+  asset,
+
+  zoom,
+
+  position,
+
+  dragging,
+
+  wheelZoom,
+
+  mouseDown,
+
+  mouseMove,
+
+  mouseUp,
+
+  doubleClick,
+
+}) {
+
+  return (
+
+    <img
+
+      src={asset.data}
+
+      alt={asset.name}
+
+      className="viewer-image"
+
+      draggable={false}
+
+      onWheel={wheelZoom}
+
+      onMouseDown={mouseDown}
+
+      onMouseMove={mouseMove}
+
+      onMouseUp={mouseUp}
+
+      onMouseLeave={mouseUp}
+
+      onDoubleClick={doubleClick}
+
+      style={{
+
+        transform:
+
+          `translate(
+
+            ${position.x}px,
+
+            ${position.y}px
+
+          )
+
+          scale(${zoom})`,
+
+        cursor:
+
+          dragging
+
+            ? "grabbing"
+
+            : "grab",
+
+      }}
+
+    />
+
+  );
+
+}
