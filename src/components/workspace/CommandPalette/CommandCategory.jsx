11b62f0162ebf1abbfd4ff9a2ebee4f84b78cfdb
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-25 22:13:07 +0530

    Add CommandCategory component with title prop

---
+++ src/components/workspace/CommandPalette/CommandCategory.jsx
@@ -0,0 +1,19 @@
+import "./CommandCategory.css";
+
+export default function CommandCategory({
+
+  title,
+
+}) {
+
+  return (
+
+    <div className="command-category">
+
+      {title}
+
+    </div>
+
+  );
+
+}
