855a42be17566f0c7e89aa8e134d1479e0263b1e
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-27 00:30:56 +0530

    Add SettingsSelect component for configurable settings

--- src/components/workspace/Settings/SettingsSelect.jsx
+++ src/components/workspace/Settings/SettingsSelect.jsx
@@ -1,1 +1,97 @@
+import "./Settings.css";
 
+export default function SettingsSelect({
+
+  label,
+
+  description,
+
+  value,
+
+  options = [],
+
+  onChange,
+
+  disabled = false,
+
+}) {
+
+  return (
+
+    <div className="settings-control">
+
+      <div className="settings-control-info">
+
+        <h4>
+
+          {label}
+
+        </h4>
+
+        {
+
+          description && (
+
+            <p>
+
+              {description}
+
+            </p>
+
+          )
+
+        }
+
+      </div>
+
+      <select
+
+        className="settings-select"
+
+        value={value}
+
+        disabled={disabled}
+
+        onChange={(event) =>
+
+          onChange(
+
+            event.target.value
+
+          )
+
+        }
+
+      >
+
+        {
+
+          options.map(
+
+            (option) => (
+
+              <option
+
+                key={option.value}
+
+                value={option.value}
+
+              >
+
+                {option.label}
+
+              </option>
+
+            )
+
+          )
+
+        }
+
+      </select>
+
+    </div>
+
+  );
+
+}
