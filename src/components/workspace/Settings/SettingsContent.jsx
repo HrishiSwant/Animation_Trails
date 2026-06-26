c22555bb9dbf34288b8f35026dd1b0d748f9a7e0
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-27 00:25:04 +0530

    Update SettingsContent.jsx

--- src/components/workspace/Settings/SettingsContent.jsx
+++ src/components/workspace/Settings/SettingsContent.jsx
@@ -1,1 +1,29 @@
+import "./Settings.css";
 
+import SettingsSection from "./SettingsSection";
+
+export default function SettingsContent({
+
+  activeSection,
+
+}) {
+
+  return (
+
+    <div className="settings-content">
+
+      <SettingsSection
+
+        activeSection={
+
+          activeSection
+
+        }
+
+      />
+
+    </div>
+
+  );
+
+}
