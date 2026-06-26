3cd6df1032a2e9580fc90bbbdd1d9d7dbfb6c9f6
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-26 23:09:32 +0530

    Add PresentationToolbar component with controls

--- src/components/presentation/PresentationToolbar.jsx
+++ src/components/presentation/PresentationToolbar.jsx
@@ -1,1 +1,117 @@
+import "./Presentation.css";
 
+export default function PresentationToolbar({
+
+  isFirst,
+
+  isLast,
+
+  isPlaying,
+
+  isFullscreen,
+
+  onPrevious,
+
+  onNext,
+
+  onRestart,
+
+  onTogglePlay,
+
+  onToggleFullscreen,
+
+  onExit,
+
+}) {
+
+  return (
+
+    <div className="presentation-toolbar">
+
+      <button
+
+        onClick={onPrevious}
+
+        disabled={isFirst}
+
+      >
+
+        ◀ Previous
+
+      </button>
+
+      <button
+
+        onClick={onNext}
+
+        disabled={isLast}
+
+      >
+
+        Next ▶
+
+      </button>
+
+      <button
+
+        onClick={onRestart}
+
+      >
+
+        ↺ Restart
+
+      </button>
+
+      <button
+
+        onClick={onTogglePlay}
+
+      >
+
+        {
+
+          isPlaying
+
+            ? "⏸ Pause"
+
+            : "▶ Play"
+
+        }
+
+      </button>
+
+      <button
+
+        onClick={onToggleFullscreen}
+
+      >
+
+        {
+
+          isFullscreen
+
+            ? "🗗 Window"
+
+            : "⛶ Fullscreen"
+
+        }
+
+      </button>
+
+      <button
+
+        className="presentation-exit"
+
+        onClick={onExit}
+
+      >
+
+        ✕ Exit
+
+      </button>
+
+    </div>
+
+  );
+
+}
