177729003b6da9073accfe5c67f18b08152de32e
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-26 19:40:21 +0530

    Implement custom hook for fullscreen functionality

--- src/hooks/presentation/useFullscreen.js
+++ src/hooks/presentation/useFullscreen.js
@@ -1,1 +1,104 @@
+import { useEffect, useState } from "react";
 
+export default function useFullscreen() {
+
+  const [isFullscreen, setIsFullscreen] =
+    useState(
+
+      !!document.fullscreenElement
+
+    );
+
+  useEffect(() => {
+
+    function handleChange() {
+
+      setIsFullscreen(
+
+        !!document.fullscreenElement
+
+      );
+
+    }
+
+    document.addEventListener(
+
+      "fullscreenchange",
+
+      handleChange
+
+    );
+
+    return () => {
+
+      document.removeEventListener(
+
+        "fullscreenchange",
+
+        handleChange
+
+      );
+
+    };
+
+  }, []);
+
+  async function enter() {
+
+    if (
+
+      !document.fullscreenElement
+
+    ) {
+
+      await document.documentElement.requestFullscreen();
+
+    }
+
+  }
+
+  async function exit() {
+
+    if (
+
+      document.fullscreenElement
+
+    ) {
+
+      await document.exitFullscreen();
+
+    }
+
+  }
+
+  async function toggle() {
+
+    if (
+
+      document.fullscreenElement
+
+    ) {
+
+      await exit();
+
+    } else {
+
+      await enter();
+
+    }
+
+  }
+
+  return {
+
+    isFullscreen,
+
+    enter,
+
+    exit,
+
+    toggle,
+
+  };
+
+}
