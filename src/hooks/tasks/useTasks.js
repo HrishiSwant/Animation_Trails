0e90ed455558931249372717b3c2819ed301e995
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-26 18:12:19 +0530

    Implement useTasks hook for task management

--- src/hooks/tasks/useTasks.js
+++ src/hooks/tasks/useTasks.js
@@ -1,1 +1,114 @@
+import { useState } from "react";
 
+import TaskManager from "../../core/tasks/TaskManager";
+
+export default function useTasks() {
+
+  const [tasks, setTasks] =
+    useState(
+      TaskManager.getTasks()
+    );
+
+  function refreshTasks() {
+
+    setTasks(
+      TaskManager.getTasks()
+    );
+
+  }
+
+  function createTask(data) {
+
+    TaskManager.createTask(
+      data
+    );
+
+    refreshTasks();
+
+  }
+
+  function updateTask(id, updates) {
+
+    TaskManager.updateTask(
+
+      id,
+
+      updates
+
+    );
+
+    refreshTasks();
+
+  }
+
+  function deleteTask(id) {
+
+    TaskManager.deleteTask(
+      id
+    );
+
+    refreshTasks();
+
+  }
+
+  function toggleCompleted(id) {
+
+    TaskManager.toggleCompleted(
+      id
+    );
+
+    refreshTasks();
+
+  }
+
+  function toggleFavorite(id) {
+
+    TaskManager.toggleFavorite(
+      id
+    );
+
+    refreshTasks();
+
+  }
+
+  function togglePinned(id) {
+
+    TaskManager.togglePinned(
+      id
+    );
+
+    refreshTasks();
+
+  }
+
+  function clearTasks() {
+
+    TaskManager.clearTasks();
+
+    refreshTasks();
+
+  }
+
+  return {
+
+    tasks,
+
+    refreshTasks,
+
+    createTask,
+
+    updateTask,
+
+    deleteTask,
+
+    toggleCompleted,
+
+    toggleFavorite,
+
+    togglePinned,
+
+    clearTasks,
+
+  };
+
+}
