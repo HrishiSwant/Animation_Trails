03f8726eeb93e9c1840f635f11bdf062265be894
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-27 00:21:44 +0530

    Implement Settings component with sidebar and content

--- src/components/workspace/Settings/Settings.jsx
+++ src/components/workspace/Settings/Settings.jsx
@@ -1,1 +1,62 @@
+import { useState } from "react";
 
+import "./Settings.css";
+
+import SettingsSidebar from "./SettingsSidebar";
+import SettingsContent from "./SettingsContent";
+
+export default function Settings() {
+
+  /*
+  ==========================
+      ACTIVE SECTION
+  ==========================
+  */
+
+  const [
+
+    activeSection,
+
+    setActiveSection,
+
+  ] = useState(
+
+    "appearance"
+
+  );
+
+  return (
+
+    <div className="settings">
+
+      <SettingsSidebar
+
+        activeSection={
+
+          activeSection
+
+        }
+
+        onChange={
+
+          setActiveSection
+
+        }
+
+      />
+
+      <SettingsContent
+
+        activeSection={
+
+          activeSection
+
+        }
+
+      />
+
+    </div>
+
+  );
+
+}
