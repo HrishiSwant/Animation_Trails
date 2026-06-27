c12d94522273e2054de564d7fec838f9ec7795f2
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 00:10:49 +0530

    Implement AccentStorage class for localStorage management

--- src/core/accent/AccentStorage.js
+++ src/core/accent/AccentStorage.js
@@ -1,1 +1,25 @@
+const STORAGE_KEY = "hrishi-studio-accent";
 
+class AccentStorage {
+
+  load() {
+
+    return localStorage.getItem(STORAGE_KEY);
+
+  }
+
+  save(color) {
+
+    localStorage.setItem(STORAGE_KEY, color);
+
+  }
+
+  clear() {
+
+    localStorage.removeItem(STORAGE_KEY);
+
+  }
+
+}
+
+export default new AccentStorage();
