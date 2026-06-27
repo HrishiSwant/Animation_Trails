9c7e287e5350ccca3983ae32d3bc7fc3b3958de7
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 00:11:15 +0530

    Add applyAccent function to set primary color

--- src/core/accent/applyAccent.js
+++ src/core/accent/applyAccent.js
@@ -1,1 +1,17 @@
+export default function applyAccent(color) {
 
+  if (!color) {
+
+    return;
+
+  }
+
+  document.documentElement.style.setProperty(
+
+    "--color-primary",
+
+    color
+
+  );
+
+}
