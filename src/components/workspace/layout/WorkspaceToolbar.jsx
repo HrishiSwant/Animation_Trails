feb1c857bc2b524b0b670171b240fde22a16b041
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 23:27:16 +0530

    Add WorkspaceToolbar component with basic structure

---
+++ src/components/workspace/layout/WorkspaceToolbar.jsx
@@ -0,0 +1,19 @@
+import "./WorkspaceToolbar.css";
+
+export default function WorkspaceToolbar() {
+
+  return (
+
+    <header className="workspace-toolbar">
+
+      <h3>
+
+        Workspace
+
+      </h3>
+
+    </header>
+
+  );
+
+}
