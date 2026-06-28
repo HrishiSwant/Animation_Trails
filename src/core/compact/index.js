3b3d59770fd0ac59c7f62c92b0593d18fa751b2d
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 17:50:13 +0530

    Add index.js to export compact module components

---
+++ src/core/compact/index.js
@@ -0,0 +1,8 @@
+export { default as CompactManager }
+from "./CompactManager";
+
+export { default as CompactStorage }
+from "./CompactStorage";
+
+export { default as applyCompact }
+from "./applyCompact";
