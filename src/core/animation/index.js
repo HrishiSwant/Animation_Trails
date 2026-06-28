1be5dd87b1321f9fe6010fae3009b89f645e660c
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 17:13:17 +0530

    Export animation modules from index.js

---
+++ src/core/animation/index.js
@@ -0,0 +1,8 @@
+export { default as AnimationManager }
+from "./AnimationManager";
+
+export { default as AnimationStorage }
+from "./AnimationStorage";
+
+export { default as applyAnimations }
+from "./applyAnimations";
