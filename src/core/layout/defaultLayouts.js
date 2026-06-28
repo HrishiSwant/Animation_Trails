206241924d58cc5758ded17ed99a5e8845db9396
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 23:11:02 +0530

    Add default layout configuration

---
+++ src/core/layout/defaultLayouts.js
@@ -0,0 +1,37 @@
+const defaultLayout = {
+
+  sidebar: {
+
+    visible: true,
+
+    width: 280,
+
+  },
+
+  inspector: {
+
+    visible: true,
+
+    width: 320,
+
+  },
+
+  statusbar: {
+
+    visible: true,
+
+    height: 34,
+
+  },
+
+  toolbar: {
+
+    visible: true,
+
+    height: 56,
+
+  },
+
+};
+
+export default defaultLayout;
