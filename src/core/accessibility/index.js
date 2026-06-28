4a137163f5ed59306a1df84a5b1bee42f2662b70
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 18:04:27 +0530

    Add accessibility module exports in index.js

---
+++ src/core/accessibility/index.js
@@ -0,0 +1,8 @@
+export { default as AccessibilityManager }
+from "./AccessibilityManager";
+
+export { default as AccessibilityStorage }
+from "./AccessibilityStorage";
+
+export { default as applyAccessibility }
+from "./applyAccessibility";
