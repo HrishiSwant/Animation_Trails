d8d845af981b0f1647d8a8ea0ae9e02b25aaed66
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-28 23:11:51 +0530

    Implement LayoutManager class for layout management

---
+++ src/core/layout/LayoutManager.js
@@ -0,0 +1,100 @@
+import defaultLayout from "./defaultLayout";
+import LayoutStorage from "./LayoutStorage";
+
+class LayoutManager {
+
+  constructor() {
+
+    this.layout = defaultLayout;
+
+    this.listeners = [];
+
+  }
+
+  initialize() {
+
+    const saved =
+
+      LayoutStorage.load();
+
+    if (saved) {
+
+      this.layout = saved;
+
+    }
+
+  }
+
+  getLayout() {
+
+    return this.layout;
+
+  }
+
+  update(part, value) {
+
+    this.layout = {
+
+      ...this.layout,
+
+      [part]: {
+
+        ...this.layout[part],
+
+        ...value,
+
+      },
+
+    };
+
+    LayoutStorage.save(
+
+      this.layout,
+
+    );
+
+    this.notify();
+
+  }
+
+  subscribe(listener) {
+
+    this.listeners.push(
+
+      listener,
+
+    );
+
+    return () => {
+
+      this.listeners =
+
+        this.listeners.filter(
+
+          l => l !== listener,
+
+        );
+
+    };
+
+  }
+
+  notify() {
+
+    this.listeners.forEach(
+
+      listener =>
+
+        listener(
+
+          this.layout,
+
+        ),
+
+    );
+
+  }
+
+}
+
+export default new LayoutManager();
