d4c1c3f78102855e665062a9bfda1f4a72833de0
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-29 21:58:12 +0530

    Add WorkspaceEventTypes for event handling

---
+++ src/core/workspace/events/WorkspaceEventTypes.js
@@ -0,0 +1,37 @@
+const WorkspaceEventTypes = {
+
+  TOOL_CHANGED:
+
+    "tool:changed",
+
+  PRESENTATION_ENTER:
+
+    "presentation:enter",
+
+  PRESENTATION_EXIT:
+
+    "presentation:exit",
+
+  PANEL_CHANGED:
+
+    "panel:changed",
+
+  PROJECT_CHANGED:
+
+    "project:changed",
+
+  DOCUMENT_CHANGED:
+
+    "document:changed",
+
+  ZOOM_CHANGED:
+
+    "zoom:changed",
+
+  SELECTION_CHANGED:
+
+    "selection:changed",
+
+};
+
+export default WorkspaceEventTypes;
