66bb1873cefe297451a5485413e8b1bafd23ce20
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-29 15:29:55 +0530

    Add defaultDockLayout configuration file

---
+++ src/core/docking/defaultDockLayout.js
@@ -0,0 +1,19 @@
+const defaultDockLayout = {
+
+  panels: {
+
+    inspector: {
+
+      dock: "right",
+
+      visible: true,
+
+      order: 0,
+
+    },
+
+  },
+
+};
+
+export default defaultDockLayout;
