0404e745ae962187bfd4d0cb2694537206feb014
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-06-29 21:29:16 +0530

    Add default workspace state configuration

---
+++ src/core/workspace/state/defaultWorkspaceState.js
@@ -0,0 +1,51 @@
+const defaultWorkspaceState = {
+
+  /*
+  ==========================
+      WORKSPACE
+  ==========================
+  */
+
+  currentTool: "dashboard",
+
+  presentation: false,
+
+  /*
+  ==========================
+      PROJECT
+  ==========================
+  */
+
+  activeProject: null,
+
+  activeDocument: null,
+
+  /*
+  ==========================
+      PANEL
+  ==========================
+  */
+
+  activePanel: null,
+
+  /*
+  ==========================
+      SELECTION
+  ==========================
+  */
+
+  selection: null,
+
+  clipboard: null,
+
+  /*
+  ==========================
+      VIEWPORT
+  ==========================
+  */
+
+  zoom: 100,
+
+};
+
+export default defaultWorkspaceState;
