99764cd971244d12c4738a39f2c03795a8c3ab58
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-01 20:36:20 +0530

    Create FavoriteToggle component
    Adds a FavoriteToggle component for toggling favorite status.

---
+++ src/components/project/ProjectCreator/components/FavoriteToggle.jsx
@@ -0,0 +1,47 @@
+export default function FavoriteToggle({
+
+  value,
+
+  onChange,
+
+}) {
+
+  return (
+
+    <label className="favorite-toggle">
+
+      <input
+
+        type="checkbox"
+
+        checked={value}
+
+        onChange={event =>
+
+          onChange(
+
+            event.target.checked,
+
+          )
+
+        }
+
+      />
+
+      <span className="favorite-icon">
+
+        ⭐
+
+      </span>
+
+      <span>
+
+        Pin this project
+
+      </span>
+
+    </label>
+
+  );
+
+}
