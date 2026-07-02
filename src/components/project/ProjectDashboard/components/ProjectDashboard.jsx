5d031e6462e1c5f7a983ac75be7380efbc9598e5
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-02 23:35:59 +0530

    Refactor ProjectDashboard component structure

--- src/components/project/ProjectDashboard/components/ProjectDashboard.jsx
+++ src/components/project/ProjectDashboard/components/ProjectDashboard.jsx
@@ -1,32 +1,131 @@
+import { useState } from "react";
+
 import "./ProjectDashboard.css";
 
 import useProject from "../../../hooks/project/useProject";
 
 import DashboardHeader from "./components/DashboardHeader";
+import ProjectRename from "../ProjectRename/ProjectRename";
 
 export default function ProjectDashboard() {
+
+  const [
+
+    renameOpen,
+
+    setRenameOpen,
+
+  ] = useState(false);
 
   const {
 
     activeProject,
 
+    projects,
+
+    renameProject,
+
+    duplicateProject,
+
+    updateProject,
+
   } = useProject();
+
+  if (!activeProject) {
+
+    return null;
+
+  }
 
   return (
 
-    <div className="project-dashboard">
+    <>
 
-      <DashboardHeader
+      <div className="project-dashboard">
 
-        project={
+        <DashboardHeader
 
-          activeProject
+          project={activeProject}
+
+          onRename={() =>
+
+            setRenameOpen(true)
+
+          }
+
+          onDuplicate={() =>
+
+            duplicateProject(
+
+              activeProject.id,
+
+            )
+
+          }
+
+          onFavorite={() =>
+
+            updateProject(
+
+              activeProject.id,
+
+              {
+
+                favorite:
+
+                  !activeProject.favorite,
+
+              },
+
+            )
+
+          }
+
+          onSettings={() =>
+
+            alert(
+
+              "Project Settings will be added in D-1.5",
+
+            )
+
+          }
+
+        />
+
+      </div>
+
+      <ProjectRename
+
+        open={renameOpen}
+
+        project={activeProject}
+
+        projects={projects}
+
+        onClose={() =>
+
+          setRenameOpen(false)
 
         }
 
+        onRename={name => {
+
+          renameProject(
+
+            activeProject.id,
+
+            name,
+
+          );
+
+          setRenameOpen(false);
+
+        }}
+
       />
 
-    </div>
+    </>
 
   );
 
