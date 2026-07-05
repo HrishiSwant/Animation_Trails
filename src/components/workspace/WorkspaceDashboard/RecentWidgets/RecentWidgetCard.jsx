1f593548f232a36de5bd29adc7cf4a6a59cc5e48
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-05 19:47:06 +0530

    Add RecentWidgetCard component

---
+++ src/components/workspace/WorkspaceDashboard/RecentWidgets/RecentWidgetCard.jsx
@@ -0,0 +1,25 @@
+import "./RecentWidgetCard.css";
+
+export default function RecentWidgetCard({
+
+  title,
+
+}) {
+
+  return (
+
+    <div className="recent-widget-card">
+
+      🕒
+
+      <span>
+
+        {title}
+
+      </span>
+
+    </div>
+
+  );
+
+}
