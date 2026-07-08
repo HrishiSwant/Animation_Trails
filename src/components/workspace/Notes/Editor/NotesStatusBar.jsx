c062cd0a946bada9548d34ca03c09c48cfcf9461
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-08 19:20:11 +0530

    Add NotesStatusBar component for note status display

---
+++ src/components/workspace/Notes/Editor/NotesStatusBar.jsx
@@ -0,0 +1,31 @@
+import "./NotesStatusBar.css";
+
+export default function NotesStatusBar() {
+
+  return (
+
+    <div className="notes-status-bar">
+
+      <span>
+
+        Words: 0
+
+      </span>
+
+      <span>
+
+        Characters: 0
+
+      </span>
+
+      <span>
+
+        Ready
+
+      </span>
+
+    </div>
+
+  );
+
+}
